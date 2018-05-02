2ad9590e70041bbc1b5e6fa44f50cc39ab8bdc99
seongjungkim <plznw4me@naver.com>
2018-05-02 15:04:08 +0900

    inverse_time_decay

--- lr_scheduler.py
+++ lr_scheduler.py
@@ -1,10 +1,13 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 lr_=tf.placeholder(tf.float32  , name = 'learning_rate')
-global_step = tf.placeholder(dtype =tf.float32)
+global_step = tf.placeholder(dtype =tf.int32)
 
 #learning_rate = tf.train.exponential_decay(0.01,global_step,decay_steps=100000,decay_rate=0.96,staircase=True)
-learning_rate = tf.train.inverse_time_decay(0.01,global_step,decay_steps=100000,decay_rate=0.96,staircase=False)
+#learning_rate = tf.train.inverse_time_decay(0.01,global_step,decay_steps=100000,decay_rate=0.96,staircase=False)
+boundaries = [10000,50000]
+values = [1.0 , 0.5 , 0.1]
+learning_rate = tf.train.piecewise_constant(global_step , boundaries , values)
 sess=tf.Session()
 sess.run(tf.global_variables_initializer())
 ys=[]
