20d53f1715b5f449c54ba08a82b5d23ed4377bdf
seongjungkim <plznw4me@naver.com>
2018-05-02 14:23:23 +0900

    exponential decay

---
+++ lr_scheduler.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+lr_=tf.placeholder(tf.float32  , name = 'learning_rate')
+global_step = tf.placeholder(dtype =tf.float32)
+
+#learning_rate = tf.train.exponential_decay(0.01,global_step,decay_steps=1000,decay_rate=0.96,staircase=True)
+learning_rate = tf.train.inverse_time_decay(0.01,global_step,decay_steps=10000,decay_rate=0.96,staircase=True)
+sess=tf.Session()
+sess.run(tf.global_variables_initializer())
+ys=[]
+maxiter=100000
+for step in range(maxiter):
+    lr=sess.run(learning_rate, {global_step: step})
+    ys.append(lr)
+print lr
+plt.plot(range(maxiter), ys)
+plt.show()
+
